cd6b1885f9efe09ca055bde7536e84f12d4163cd
Anwesh43 <anweshthecool0@gmail.com>
2018-08-02 00:13:03 +0530

    importing libnraries

--- brickbuilderview/src/main/java/com/anwesh/uiprojects/brickbuilderview/BrickBuilderView.kt
+++ brickbuilderview/src/main/java/com/anwesh/uiprojects/brickbuilderview/BrickBuilderView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.brickbuilderview
 /**
  * Created by anweshmishra on 02/08/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+
+val nodes : Int = 5
+
